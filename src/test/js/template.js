1846b1af6923f91e1ed9e7429ed1e2f2c6a9dc55
Manuel Leuenberger <maenulabs@gmx.ch>
2015-01-23 18:28:51 +0100

    remove empty lines

--- src/test/js/template.js
+++ src/test/js/template.js
@@ -190,9 +190,7 @@ exports['template'] = {
 					'should store instrumented in temp directory');
 			var instrumented = this.context.scripts.src[0];
 			var found = grunt.file.read(instrumented).split('\n')[1];
-
 			var expected = "var __cov_IWp9qM1GTBo1x3ZChf8cEQ = (Function('return this'))();";
-
 			test.equal(found, expected, 'should be instrumented');
 			test.done();
 		},
